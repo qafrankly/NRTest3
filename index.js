04ae5c2e7585d69387c7ce080c8d090064d2c0ea
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-19 12:30:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+                        "id": "components/social/TwitterFeed",
+                        "FRN_style": {}
+                        "props": {
+                            "title": "Twitter News",
+                            "screenName": "frankly_inc",
+                            "widgetId": "735176068399652865"
+                        }
+}
