16fe4af998f36b26d830560d904af7cd2bf13b76
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-19 16:24:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ class HomePageTakeover extends Component {
   constructor(props){ //gives us acces to props, fires long before page load
     super(props) //assigns props to this.props
     this.state = {} /* great place to assign default state */
+  }
   
-  render() { //REQUIRED
+  render(){ //REQUIRED
     return (<div className="blah">TEST/div>) 
             }
 }
-
 export default HomePageTakeover;
