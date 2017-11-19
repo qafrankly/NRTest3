7dcf5f034f32132e81ed11275af4b1e7e9268e62
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-19 16:28:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ class HomePageTakeover extends Component {
     this.state = {} /* great place to assign default state */
   }
   
-  render(){ //REQUIRED
-    return (<div className="blah">TEST</div>) 
-            }
+  render () 
+  { return (<div className="blah">test</div>); } 
 }
 export default HomePageTakeover;
             
